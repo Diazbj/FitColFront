988a1bab659e6240dd7fb05a3aea07afb42f5406
Jordy Diaz <diaz.jordyb@gmail.com>
2025-05-05 19:04:00 -0500

    registroEntrenador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './paginas/inicio/inicio.component';
 import { LoginComponent } from './paginas/login/login.component';
 import { RegistroClienteComponent} from './paginas/registro-cliente/registro-cliente.component';
+import { RegistroEntrenadorComponent } from './paginas/registro-entrenador/registro-entrenador.component';
 
 
 export const routes: Routes = [
    { path: '', component: InicioComponent },
    { path: 'login', component: LoginComponent },
    { path: 'registroCliente', component: RegistroClienteComponent },
+   {path:'registroEntrenador',component:RegistroEntrenadorComponent},
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
---
+++ src/app/paginas/registro-entrenador/registro-entrenador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-entrenador',
+  imports: [],
+  templateUrl: './registro-entrenador.component.html',
+  styleUrl: './registro-entrenador.component.css'
+})
+export class RegistroEntrenadorComponent {
+
+}
